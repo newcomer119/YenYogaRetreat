0c5b88dd8ed28fb24d3318b707e2bc87b0f8f0a5
Devansh <devansh2618@outlook.com>
2024-11-17 00:59:10 +0530

    Fixed Hero Responsiveness

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -6,14 +6,12 @@ import { Parallax } from "react-parallax";
 // import images
 import Image from "../assets/img/hero/guy4.png";
 
-
 const Hero = () => {
   return (
     <section className="min-h-[[618px] lg:min-h-[815px] pt-9 lg:bg-circle lg:bg-no-repeat lg:bg-right-top">
-      <div className="container mx-auto ">
-       
-        <div className="flex flex-col items-center lg:flex-row lg:items-start pt-32">
-          <div className=" flex-1 lg:mt-12">
+      <div className="lg:pl-32 mx-auto">
+        <div className="flex flex-col items-center lg:flex-row lg:items-start pt-10 lg:pt-32">
+          <div className=" flex-1 mt-12 mx-10 md:ml-0">
             <h1
               className="h1 mb-3 lg:mb-[22px]"
               data-aos="fade-down"
@@ -24,23 +22,22 @@ const Hero = () => {
             <p
               className="mb-6 lg:mb-12 max-w-[480px] lg:text-lg"
               data-aos="fade-down"
-              data-aos-delay="600"
+              data-aos-delay="500"
             >
               Experience transformative yoga in the heart of Vietnam, or join us
-              online from anywhere in the world. Book today and
-              embark on a journey of wellness.
+              online from anywhere in the world. Book today and embark on a
+              journey of wellness.
             </p>
             {/* btn group */}
-            <div className="mb-12 space-x-4">
-              <button className="btn btn-sm lg:btn-lg btn-orange">
-                Get Started
-              </button>
+            <div className="mb-6 md:mb-12 space-x-4">
               <button
-                className="btn btn-sm lg:btn-lg btn-orange
-           text-heading bg-transparent-border border-stroke-1 px-4
-            hover:bg-stroke-3 transition"
+                className="btn btn-sm lg:btn-lg btn-orange hover:text-lg text-white bg-transparent-border border-stroke-1  
+              hover:bg-stroke-3 hover:shadow-lg hover:-translate-y-1 hover:text-orange transition-all duration-300"
               >
-                Learn More
+                Book Now
+              </button>
+              <button className="btn btn-sm lg:btn-lg bg-egreen hover:text-lg text-white hover:bg-[#7a8f3f] hover:shadow-lg hover:-translate-y-1 transition-all duration-300">
+                Gallery {">"}
               </button>
             </div>
           </div>
