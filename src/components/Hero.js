2db034a40d83aa5e50150affa7e8585bb9710ca4
Devansh <devansh2618@outlook.com>
2024-11-17 04:23:47 +0530

    Added a Language Switcher in Hero

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -5,13 +5,15 @@ import { Parallax } from "react-parallax";
 
 // import images
 import Image from "../assets/img/hero/guy.png";
+import LanguageSwitcher from "./LanguageSwitcher";
 
 const Hero = () => {
   return (
     <section className="sm:min-h-[450px] lg:min-h-[500px] pt-9 lg:bg-circle lg:bg-no-repeat lg:bg-right-top">
+      <LanguageSwitcher/>
       <div className="lg:pl-32 mx-auto">
         <div className="flex flex-col items-center lg:flex-row lg:items-start pt-10 lg:pt-32 xxl:max-w-[70%]">
-          <div className=" flex-1 mt-12 mx-8 md:mx-0">
+          <div className=" flex-1 mt-12 mx-8 md:mx-0 lg:min-w-[50%] ">
             <h1
               className="h1 mb-3 lg:mb-[22px]"
               data-aos="fade-down"
@@ -20,7 +22,7 @@ const Hero = () => {
               Reconnect with Mind, Body and Soul
             </h1>
             <p
-              className="mb-6 lg:mb-12 max-w-[60%] sm:max-w-[500px] lg:text-lg"
+              className="mb-6 lg:mb-12 max-w-[60%] sm:max-w-full lg:text-lg"
               data-aos="fade-down"
               data-aos-delay="500"
             >
