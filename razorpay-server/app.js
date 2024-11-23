2832c4d7ee7a15d470bf35a670085dfa1ebc9ede
newcomer119 <mitarth.125035@stu.upes.ac.in>
2024-11-23 15:58:23 +0530

    added change

--- razorpay-server/app.js
+++ razorpay-server/app.js
@@ -10,7 +10,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(cors({
-  origin: "*",
+  origin: 'https://yen-yoga-retreat-dox8.vercel.app'
 }));
 app.use(bodyParser.json());
 app.use("/api/payment", paymentRoutes);
